15d0df19143f48bcf393e31c71e279dc31a94173
Adrian Hawrylak <adrianhawrylak@MacBook-Air-Adrian.local>
2017-08-11 09:56:50 +0200

    Handle 2 bottles

--- beer-song.js
+++ beer-song.js
@@ -1,7 +1,16 @@
 module.exports = function(){
+  var first_line = function(bottles){
+    return `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`;
+  }
+
+  var second_line = function(bottles){
+    var bottles_pluralized = bottles > 1 ? "bottles" : "bottle"
+    return `\nTake one down and pass it around, ${bottles} ${bottles_pluralized} of beer on the wall.\n`;
+  }
+
   return {
     verse: function(verse){
-      return '8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n';
+      return first_line(verse) + second_line(verse - 1);
     }
   };
 };
