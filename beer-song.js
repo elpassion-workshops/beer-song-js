b3f18f09d0765753caabe9bafc71e6e8cc0b89a6
Małgorzata Szporer <malgorzataszporer@MacBook-Air-Magorzata.local>
2017-08-11 10:04:03 +0200

    handles 2 bottles spec

--- beer-song.js
+++ beer-song.js
@@ -3,7 +3,13 @@ module.exports = function(){
 
 
     verse: function(verse){
-      return `${verse} bottles of beer on the wall, ${verse} bottles of beer.\nTake one down and pass it around, ${verse - 1} bottles of beer on the wall.\n`
+      switch(verse) {
+        case 2:
+          return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
+          break;
+        default:
+          return `${verse} bottles of beer on the wall, ${verse} bottles of beer.\nTake one down and pass it around, ${verse - 1} bottles of beer on the wall.\n`
+      }
     }
   };
 };
