b5add686496afbf50729072c2430fb2615f079c1
Tomy Hirao <tomyhirao@MacBook-Air-Tomy.local>
2017-08-11 09:51:20 +0200

    add interpolation to verse

--- beer-song.js
+++ beer-song.js
@@ -1,7 +1,7 @@
 module.exports = function(){
   return {
     verse: function(verse){
-      return '8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n';
+      return `${verse} bottles of beer on the wall, ${verse} bottles of beer.\nTake one down and pass it around, ${verse - 1} bottles of beer on the wall.\n`;
     }
   };
 };
