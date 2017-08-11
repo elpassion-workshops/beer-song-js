b89cbd367cada4584462471a915b95a5c8bc68c7
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-08-11 09:53:46 +0200

    handles 1 bottle

--- beer-song.js
+++ beer-song.js
@@ -4,16 +4,27 @@ module.exports = function(){
     switch(number){
       case 1:
         return number+" bottle"
-        break;
       default:
         return number+" bottles"
-        break;
+    }
+  }
+
+  var _first_verse = function(number){
+    return _format(number) + " of beer on the wall, " + _format(number) +" of beer.\n"
+  }
+
+  var _last_verse = function(number){
+    switch(number){
+      case 1:
+        return "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      default:
+        return "Take one down and pass it around, "+_format(number - 1)+" of beer on the wall.\n"
     }
   }
 
   return {
     verse: function(verse){
-      return _format(verse) + " of beer on the wall, " + _format(verse) +" of beer.\nTake one down and pass it around, "+_format(verse - 1)+" of beer on the wall.\n"
+      return _first_verse(verse) + _last_verse(verse)
     }
   };
 };
