494b88dd266c9f13a1d2ed5636421a769c2d1a7e
Tomy Hirao <tomyhirao@MacBook-Air-Tomy.local>
2017-08-11 10:31:11 +0200

    add secound_verse function to verse

--- beer-song.js
+++ beer-song.js
@@ -1,15 +1,25 @@
 module.exports = function(){
-	function format(verse) {
-		if (verse == 1) {
-			return `${verse} bottle`;
-		} else {
-			return `${verse} bottles`;
-		}
-	}
+  
+  function first_verse(verse) {
+    if (verse === 1){
+      return `${verse} bottle`;
+    } else 
+      return `${verse} bottles`;
+  }
+
+  function secound_verse(verse) {
+    if (verse === 1) {
+      return `Take one down and pass it around, ${verse} bottle of beer on the wall.\n`;
+     } else if (verse === 0) {
+        return `Take it down and pass it around, no more bottles of beer on the wall.\n`;
+     } else {
+        return `Take one down and pass it around, ${verse} bottles of beer on the wall.\n`;
+        }
+  }
 
   return {
     verse: function(verse){
-      return `${verse} bottles of beer on the wall, ${verse} bottles of beer.\nTake one down and pass it around, ${format(verse - 1)} of beer on the wall.\n`;
+      return `${first_verse(verse)} of beer on the wall, ${first_verse(verse)} of beer.\n${secound_verse(verse - 1)}`;
     }
   };
 };
