6af81f429b9c45d2e86db7721b8b56e3a914c9ca
Bartosz Kowalski <bartoszkowalski@MacBook-Air-Bartosz.local>
2017-08-11 10:03:12 +0200

    handles 0 bottles

--- beer-song.js
+++ beer-song.js
@@ -3,28 +3,35 @@ module.exports = function(){
   var _format = function(number){
     switch(number){
       case 1:
-        return number+" bottle"
+        return `${number} bottle`
       default:
-        return number+" bottles"
+        return `${number} bottles`
     }
   }
 
   var _first_verse = function(number){
-    return _format(number) + " of beer on the wall, " + _format(number) +" of beer.\n"
+    switch(number) {
+      case 0:
+        return "No more bottles of beer on the wall, no more bottles of beer.\n"
+      default:
+        return `${_format(number)} of beer on the wall, ${_format(number)} of beer.\n`
+    }
   }
 
   var _last_verse = function(number){
     switch(number){
-      case 1:
+      case 0:
         return "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      case -1:
+        return "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
       default:
-        return "Take one down and pass it around, "+_format(number - 1)+" of beer on the wall.\n"
+        return `Take one down and pass it around, ${_format(number)} of beer on the wall.\n`
     }
   }
 
   return {
     verse: function(verse){
-      return _first_verse(verse) + _last_verse(verse)
+      return _first_verse(verse) + _last_verse(verse - 1)
     }
   };
 };
