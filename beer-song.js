7ca981f5cfd039ff48bcd66179fb8979b3d4046c
Małgorzata Szporer <malgorzataszporer@MacBook-Air-Magorzata.local>
2017-08-11 10:09:32 +0200

    handles 1 bottle spec

--- beer-song.js
+++ beer-song.js
@@ -5,8 +5,9 @@ module.exports = function(){
     verse: function(verse){
       switch(verse) {
         case 2:
-          return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
-          break;
+          return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+        case 1:
+          return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
         default:
           return `${verse} bottles of beer on the wall, ${verse} bottles of beer.\nTake one down and pass it around, ${verse - 1} bottles of beer on the wall.\n`
       }
